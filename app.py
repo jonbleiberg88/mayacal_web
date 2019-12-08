e21a2c3a14cf3a785c5e4e8c8a4458f729b22160
jonbleiberg88 <jlbleiberg@gmail.com>
2019-12-08 00:57:39 -0500

    adding prev files

--- app.py
+++ app.py
@@ -1,13 +1,34 @@
-from flask import Flask
+from flask import Flask, request, jsonify, render_template
 import mayacal as mc
 
 app = Flask(__name__)
 
 api_route = '/api/v1/'
 
+@app.route('/')
+def index():
+    render_template('index.html')
+
+
 @app.route(f'{api_route}infer', methods=['POST'])
 def infer():
-    return
+    req_json = request.get_json()
+
+    try:
+        date = mc.mayadate.from_dict(req_json)
+        poss_dates = date.infer_mayadates()
+        poss_dates = [d.to_dict for d in poss_dates]
+        response_dict = {
+            'status' : "success",
+            'data' : {'poss_dates' : poss_dates}}
+
+    except:
+        response_dict = {
+            'status' : "error",
+            'message' : "Unable to infer date components"
+        }
+
+    return jsonify(response_dict)
 
 @app.route(f'{api_route}convert/from_maya', methods=['POST'])
 def convert_from_maya():
